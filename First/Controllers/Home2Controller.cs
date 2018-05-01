6d585c86e155e55cb845e83621dc99d35c95aac3
Wei Xia <wei_xia@hotmail.com>
2018-04-30 20:32:01 -0400

    initiate setup

---
+++ First/Controllers/Home2Controller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using First.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace First.Controllers
+{
+    public class Home2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Greeting = DateTime.Now.ToLongTimeString();
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        public ViewResult RsvpForm(GuestResponse response)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(response);
+                return View("Thanks", response);
+            }
+            else
+                return View();
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.getReponses().Where(r => r.WillAttend==true));
+        }
+    }
+}
---
+++ First/Models/Repository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace First.Models
+{
+    public static class Repository
+    {
+        static List<GuestResponse> listResponses;
+        static Repository()
+        {
+            listResponses = new List<GuestResponse>();
+        }
+
+        public static IEnumerable<GuestResponse> getReponses()
+        {
+            return listResponses;
+        }
+        public static void  AddResponse(GuestResponse response)
+        {
+            listResponses.Add(response);
+        }
+    }
+}
